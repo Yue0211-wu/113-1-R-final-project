14aa7410dc359bff9b5ccc700f1aea0a2130983b
Yue0211-wu <cmea2gg2@gmail.com>
2024-12-07 08:22:20 +0000

    測試

---
+++ week13.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+
+# 導入 CSV 檔案
+taipei_agencies <- read_csv("data/台北市仲介公司資料.csv", locale = locale(encoding = "UTF-8"))
+
+glimpse(taipei_agencies)
+
+# 統計 "評鑑等級" 類別及數量
+taipei_agencies |>
+  dplyr::count(`評鑑等級`, name = "數量")
+
+# 提取 "機構地址" 中的區名稱
+taipei_agencies |>
+  dplyr::mutate(
+    區 = stringr::str_extract(`機構地址`, "([\\w]+區)")  # 假設區域名稱以"區"結尾
+  ) |>
+  dplyr::count(`區`, name = "數量") |>
+  dplyr::arrange(desc(數量))
+
